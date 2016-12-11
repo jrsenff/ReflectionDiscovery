a0ea115dfb85358b8093f3e0c3cc075fb6adc2ac
Jerry Senff <jrsenff@hotmail.com>
2016-12-11 11:48:08 -0800

    Checking in ReflectionDiscovery project.

---
+++ ReflectionDiscovery/ReflectionDiscovery/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReflectionDiscovery
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nBegin Reflection Discovery\n");
+            Console.WriteLine("Select a .DLL file containing an assembly manifest...");
+
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.InitialDirectory = "c:\\";
+            ofd.Multiselect = false;
+            ofd.Title = "Select .DLL Assembly";
+            ofd.Filter = "Dynamic Link Library | *.dll";
+            ofd.ShowDialog();
+            string assembly = ofd.FileName;
+            Console.WriteLine("\nAssembly chosen: \n");
+            Console.WriteLine(assembly + "\n");
+
+            try
+            {
+                Assembly a = Assembly.LoadFrom(assembly);
+                Console.WriteLine("\nAssembly name = " + a.GetName());
+
+                Type[] tarr = a.GetTypes();
+                BindingFlags flags = BindingFlags.NonPublic | BindingFlags.Public |
+                                     BindingFlags.Static | BindingFlags.Instance;
+
+                foreach (Type t in tarr)
+                {
+                    Console.WriteLine("  Type name = " + t.Name + "\n");
+                    MemberInfo[] members = t.GetMembers(flags);
+                    foreach (MemberInfo mi in members)  // fields, methods, ctors, etc.
+                    {
+                        if (mi.MemberType == MemberTypes.Field)
+                            Console.WriteLine("  (Field) member name = " + mi.Name);
+                    } 
+
+                    MethodInfo[] miarr = t.GetMethods();  // public only
+                    foreach (MethodInfo mi in miarr)
+                    {
+                        Console.WriteLine("   Method name = " + mi.Name);
+                        Console.WriteLine("    Return type = " + mi.ReturnType);
+                        ParameterInfo[] piarr = mi.GetParameters();
+                        foreach (ParameterInfo pi in piarr)
+                        {
+                            Console.WriteLine("      Parameter name = " + pi.Name);
+                            Console.WriteLine("      Parameter type = " + pi.ParameterType + "\n");
+                        }
+                    } 
+                } 
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("\nFatal error: " + ex.Message);
+                Console.WriteLine("  Error data: " + ex.Data);
+                Console.WriteLine("  Error source: " + ex.Source);
+            }
+            Console.WriteLine("\n\nDone! Press Enter to exit...");
+            Console.ReadLine();
+        } 
+    }
+}
---
+++ ReflectionDiscovery/ReflectionDiscovery/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ReflectionDiscovery")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ReflectionDiscovery")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("604ddd5d-4ee4-4ff0-b6e5-ade1cf3299a2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
